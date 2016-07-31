16bbbed5e1beb84770335d48dde0f5c08bb5208e
Richard Stephenson <rstephen@thoughtworks.com>
2016-07-31 14:15:46 +0100

    Updated library files

--- dev/js/app/config/routes.js
+++ dev/js/app/config/routes.js
@@ -10,10 +10,47 @@ window.app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvi
         }
     )
 
-    .state('page',
+    .state('post',
         {
-            url: '/page',
-            templateUrl: 'templates/page.html'
+            url: '/post/:id',
+            templateUrl: 'templates/post.html'
+        }
+    )
+
+    .state('login',
+        {
+            url: '/',
+            templateUrl: 'templates/login.html'
+        }
+    )
+
+    .state('admin',
+        {
+            url: '/',
+            templateUrl: 'templates/admin.html',
+            access: {
+                requiresLogin: true
+            }
+        }
+    )
+
+    .state('add-post',
+        {
+            url: '/',
+            templateUrl: 'templates/add-post.html',
+            access: {
+                requiresLogin: true
+            }
+        }
+    )
+
+    .state('edit-post',
+        {
+            url: '/',
+            templateUrl: 'templates/edit-post.html',
+            access: {
+                requiresLogin: true
+            }
         }
     );
 
